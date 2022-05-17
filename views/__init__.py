dea71881d2bd680c4d9842ce1cca0ecfb05a1f26
jibjiby <my01sun@gmail.com>
2022-05-17 15:01:32 +0900

    update

--- views/__init__.py
+++ views/__init__.py
@@ -3,11 +3,6 @@ from flask import Flask
 from . import main_views, auth_views, user_views, bestseller_views, book_views, likeslog_views
 
 
-# from . import post_views
-
-
-
-
 def create_endpoints(app: Flask):
   app.register_blueprint(main_views.bp)
   app.register_blueprint(auth_views.bp)
@@ -15,5 +10,3 @@ def create_endpoints(app: Flask):
   app.register_blueprint(book_views.bp)
   app.register_blueprint(bestseller_views.bp)
   app.register_blueprint(likeslog_views.bp)
-
-  # app.register_blueprint(post_views.bp)
